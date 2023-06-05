f987b0870f8d8124d57de72f4588c78b66e49fb1
izemaghilas <saidtala23@gmail.com>
2023-06-05 21:32:26 +0200

    add app router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,19 @@
+import {
+    Navigate,
+    Route,
+    BrowserRouter as Router,
+    Routes,
+} from "react-router-dom";
+
 function App() {
-    return <div>CARBON</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<div>page d'acceuil</div>} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
