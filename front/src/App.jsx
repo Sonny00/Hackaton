aa6ec0f8ddc3182670dea8b5582513897e45c8d7
izemaghilas <saidtala23@gmail.com>
2023-06-06 16:06:13 +0200

    css in js

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,9 +4,24 @@ import {
     BrowserRouter as Router,
     Routes,
 } from "react-router-dom";
+import { createUseStyles } from "react-jss";
 import { UserProvider } from "./contexts/UserProvider";
 
+const useColors = createUseStyles({
+    "@global": {
+        ":root": {
+            "--color-black": "#282B2A",
+            "--color-white": "#FDFDFD",
+            "--color-red": "#E53F49",
+            "--color-green": "#00BB7E",
+            "--color-blue": "#5B98D2",
+        },
+    },
+});
+
 function App() {
+    useColors();
+    
     return (
         <UserProvider>
             <Router>
