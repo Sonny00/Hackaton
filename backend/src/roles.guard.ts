45d9693e5c22139280bd0033e6e9d6ae0e0896d2
Mustakim-Khan <mustakimzidan@gmail.com>
2023-06-06 17:21:03 +0200

    clean role guard

--- backend/src/roles.guard.ts
+++ backend/src/roles.guard.ts
@@ -17,10 +17,6 @@ export class RolesGuard implements CanActivate {
         }
         const request = context.switchToHttp().getRequest();
         const user = request.user;
-        console.log("user : " + user);
-        
         return this.matchRoles(roles, user.role);
-
-
     }
 }
