22804d7a16f68c4e029270d7f3a30425ae13cc13
izemaghilas <saidtala23@gmail.com>
2023-06-07 22:32:13 +0200

    (back) owner or admin guard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -7,45 +7,46 @@ import {
   Post,
   Put,
   UseGuards,
-  SetMetadata,
   UseInterceptors,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
-import { Roles } from 'src/roles.decorator';
 import { UsersInterceptor } from 'src/interceptors/users.interceptor';
+import { IsOwnerOrAdmin } from 'src/is-admin-or-owner.decorator';
+import { OwnerOrAdminGuard } from 'src/owner-or-admin.gurad';
+import { Roles } from 'src/roles.decorator';
 import { RolesGuard } from 'src/roles.guard';
 import { CreateUserDto } from './userDto/users.dto';
 import { UsersService } from './users.service';
+import { Entities } from 'src/entity.enum';
 
 @Controller('users')
 @UseInterceptors(UsersInterceptor)
+@UseGuards(AuthGuard('jwt'), RolesGuard, OwnerOrAdminGuard)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
-  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles('ADMIN')
   async getUsers(): Promise<User[]> {
     return this.userService.getUsers();
   }
 
   @Get(':id')
-  // TODO: Add authorization
+  @IsOwnerOrAdmin('User')
   async getUserById(@Param('id') id: string): Promise<User | null> {
     return this.userService.findUserById(id);
   }
 
   @Post()
-  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles('ADMIN')
   async createUser(@Body(ValidationPipe) user: CreateUserDto): Promise<User> {
     return await this.userService.create(user);
   }
 
   @Put(':id')
-  // TODO: Add authorization
+  @IsOwnerOrAdmin('User')
   async updateUser(
     @Param('id') id: string,
     @Body() user: User,
@@ -54,7 +55,7 @@ export class UsersController {
   }
 
   @Delete(':id')
-  // TODO: Add authorization
+  @IsOwnerOrAdmin(Entities.USER)
   async deleteUser(@Param('id') id: string): Promise<void> {
     await this.userService.deleteUser(id);
   }
